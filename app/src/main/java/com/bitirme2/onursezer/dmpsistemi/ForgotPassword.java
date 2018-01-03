539acf3e463550a3d608a48e972f80450497433a
Onur SEZER <onursezer03@gmail.com>
2018-01-03 21:10:16 +0300

    up

--- app/src/main/java/com/bitirme2/onursezer/dmpsistemi/ForgotPassword.java
+++ app/src/main/java/com/bitirme2/onursezer/dmpsistemi/ForgotPassword.java
@@ -1,23 +1,20 @@
 package com.bitirme2.onursezer.dmpsistemi;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-
-import com.bitirme2.onursezer.dmpsistemi.R;
 
 public class ForgotPassword extends AppCompatActivity implements View.OnClickListener {
 
@@ -71,11 +68,11 @@ public class ForgotPassword extends AppCompatActivity implements View.OnClickLis
                         public void onComplete(@NonNull Task<Void> task) {
                             if(task.isSuccessful())
                             {
-                                Snackbar snackBar = Snackbar.make(activity_forgot,"We have sent password to email: "+email,Snackbar.LENGTH_SHORT);
+                                Snackbar snackBar = Snackbar.make(activity_forgot,"E-postaya şifre gönderdik: "+email,Snackbar.LENGTH_SHORT);
                                 snackBar.show();
                             }
                             else{
-                                Snackbar snackBar = Snackbar.make(activity_forgot,"Failed to send password",Snackbar.LENGTH_SHORT);
+                                Snackbar snackBar = Snackbar.make(activity_forgot,"Şifre gönderilemedi!",Snackbar.LENGTH_SHORT);
                                 snackBar.show();
                             }
                         }
